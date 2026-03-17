e36e32f8f9d2cbea2d29692d181dcf0901c90b0c
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-17 16:11:08 +0530

    Create f 18 .c

---
+++ f 18 .c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int gcd(int a,int b){
+    while (b!=0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+    }
+    return a;
+}
+
+int main(){
+    int a,b;
+    scanf("%d%d",&a,&b);
+    printf("%d",gcd(a,b));
+    return a,b;
+}
