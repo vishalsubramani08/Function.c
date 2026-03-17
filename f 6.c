a52851cbbc6c3c1de97dec142ddc3abe8357a12d
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-17 11:43:29 +0530

    Create f 6.c

---
+++ f 6.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int factorial(int n){
+     int i, fact=1;
+     for (i=1;i<=n;i++){
+         fact= fact*i;
+     }
+     return fact;
+ }
+ int main(){
+     int n;
+     scanf("%d",&n);
+     printf("%d",factorial(n));
+     return 0;
+ }
