0af65d7838cf68819353c5ece633e86a08cd53dd
vishalsubramani08 <vishalsubramani08@gmail.com>
2026-03-17 15:47:54 +0530

    Create f 14 .c

---
+++ f 14 .c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int  perfect(int n){
+    int sum=0;
+    for (int i=1;i<=n;i++){
+        if(n%i==0)
+        sum=sum+i;
+}
+if (sum==n)
+     return 1;
+     else
+     return 0;
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    if(perfect(n))
+    printf("Perfect Number");
+    else 
+    printf("Not Perfect Number");
+    return 0;
+}
